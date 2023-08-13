38d7bd77fa646baac7cd40827c65a4bae9c70098
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-08-13 20:46:12 +0900

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
+// tailwind.config.js
 const { nextui } = require('@nextui-org/react')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,13 +10,7 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   darkMode: 'class',
   plugins: [nextui()],
