542827e3eba2d62e51a20eae7f35fb215aae8937
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-08-14 23:12:09 +0900

    page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,19 +1,29 @@
 export const runtime = 'edge'
-import { client } from '@/libs/client'
 import React from 'react'
 import { Article, ArticleType } from './Article'
+import { createClient } from "microcms-js-sdk";
+
+async function getContents() {
+  const client = createClient({
+   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN ?? '',
+   apiKey:process.env.MICROCMS_API_KEY ?? '',
+  });
+ 
+  const response = await client.getList({
+   customRequestInit: {
+    cache: "no-store", // キャッシュを利用せずに常に新しいデータを取得する
+   },
+   endpoint: "articles",
+  });
+ 
+  return response.contents;
+ }
 
 export default async function page() {
-  const data = await client.getList<ArticleType>({
-    endpoint: 'articles',
-    customRequestInit: {
-      cache: 'no-store',
-    },
-  })
-  // console.log(data)
+  const contents = await getContents()
   return (
     <div className="pt-10 px-2 space-y-10 max-w-2xl mx-auto">
-      {data.contents.map((article) => {
+      {contents.map((article) => {
         return <Article key={article.id} {...article} />
       })}
     </div>
