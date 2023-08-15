cb1b551266099a8b80b8e286d68684f357926aea
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-08-15 09:30:43 +0900

    cache

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -7,7 +7,7 @@ export default async function page() {
   const data = await client.getList<ArticleType>({
     endpoint: 'articles',
     customRequestInit: {
-      cache: 'default',
+      cache: 'force-cache',
     },
   })
   // console.log(data)
