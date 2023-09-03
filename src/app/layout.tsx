d6ed524d86001911166c515a1a4caad2b47e460d
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-09-03 16:10:06 +0900

    ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,11 @@ export const metadata = {
     card: "summary_large_image",
     title: siteName,
     description,
-    site: "@サイト用アカウントのTwitterID",
-    creator: "@作者のTwitterID",
+    site: "@kkhs0603",
+    creator: "@kkhs0603",
   },
   verification: {
-    google: "サーチコンソールのやつ",
+    google: "",
   },
   alternates: {
     canonical: url,
@@ -42,10 +42,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja" className={inter.className}>
+      <meta property="og:image" content="<generated>" />
+      <meta property="og:image:type" content="<generated>" />
+      <meta property="og:image:width" content="<generated>" />
+      <meta property="og:image:height" content="<generated>" />
       <body>
         <Providers>
           <Navbar />
-          <div className="max-w-[1024px] mx-auto ">{children}</div>
+          <div className="max-w-[1024px] mx-auto">{children}</div>
           <Footer />
         </Providers>
       </body>
