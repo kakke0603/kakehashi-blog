6f9bf3b62ff98eab0a91da67bb5e9c591a32741b
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-09-03 16:57:36 +0900

    ogp

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -3,16 +3,10 @@ import { client } from "@/libs/client"
 import { Article } from "./Article"
 import { ArticleType } from "../ArticleCard"
 import { Metadata } from "next"
-export const revalidate = 600
-
-type Props = {
-  params: { id: string }
-}
-
 /**
  * メタデータの設定
  */
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const { title } = await client
     .get({
       endpoint: "articles",
