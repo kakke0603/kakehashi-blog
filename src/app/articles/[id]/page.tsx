5a853076b81be2c83d0b96a1135fd38c1fce1cca
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-09-03 16:46:43 +0900

    ogp

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -2,23 +2,26 @@ export const runtime = "edge"
 import { client } from "@/libs/client"
 import { Article } from "./Article"
 import { ArticleType } from "../ArticleCard"
+import { Metadata } from "next"
+export const revalidate = 600
 
-// type Props = {
-//   params: { id: string }
-// }
-// /**
-//  * メタデータの設定
-//  */
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const { title } = await client
-//     .get({
-//       endpoint: "articles",
-//       contentId: params.id,
-//     })
-//     .then((res) => res)
-//   // templateを設定しているので、サイト名は自動で付く
-//   return { title }
-// }
+type Props = {
+  params: { id: string }
+}
+
+/**
+ * メタデータの設定
+ */
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { title } = await client
+    .get({
+      endpoint: "articles",
+      contentId: params.id,
+    })
+    .then((res) => res)
+  // templateを設定しているので、サイト名は自動で付く
+  return { title }
+}
 export default async function Page({ params }: { params: { id: string } }) {
   const data: ArticleType = await client
     .get({
