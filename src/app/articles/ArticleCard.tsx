fcf5eb83b10945d162355f96d84442ee994146b0
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-09-04 22:41:55 +0900

    article

--- src/app/articles/ArticleCard.tsx
+++ src/app/articles/ArticleCard.tsx
@@ -24,31 +24,33 @@ export const ArticleCard: React.FC<ArticleType> = ({ id, title, content, publish
   const emoji = twemoji.parse(tag)
   return (
     <Card className="py-4 w-full" isPressable onPress={handleClick}>
-      <CardBody className="py-2 gap-x-4 flex flex-row">
-        <div className="w-[70px] h-[70px] p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
-          <div dangerouslySetInnerHTML={{ __html: emoji }}></div>
-        </div>
-        <div className="space-y-2 flex-1">
-          <h4 className="font-bold text-large block">{title}</h4>
-          <HtmlStringToText htmlString={content.slice(0, 180)} />
-          <div className="flex gap-x-2">
-            {tags?.map((tag) => (
-              <Chip key={tag.id} color="default" size="sm">
-                {tag.name}
-              </Chip>
-            ))}
+      <CardBody>
+        <div className="gap-x-4 flex flex-row gap-y-4">
+          <div className="w-[70px] h-[70px] p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
+            <div dangerouslySetInnerHTML={{ __html: emoji }}></div>
           </div>
-          <div className="flex gap-x-4 flex-wrap gap-y-1 justify-end">
-            {revisedAt !== publishedAt && (
-              <div className="flex items-center">
-                <RefreshIcon />
-                <DateTime value={revisedAt} className="ml-2" />
-              </div>
-            )}
-            <div className="flex items-center">
-              <PencilIcon />
-              <DateTime value={publishedAt} className="ml-2" />
+          <div className="space-y-4 flex-1">
+            <h4 className="font-bold text-large block">{title}</h4>
+            <HtmlStringToText htmlString={content.slice(0, 180)} />
+            <div className="flex gap-x-2 overflow-scroll">
+              {tags?.map((tag) => (
+                <Chip key={tag.id} color="default" size="sm" className="whitespace-nowrap">
+                  {tag.name}
+                </Chip>
+              ))}
             </div>
+          </div>
+        </div>
+        <div className="mt-4 flex gap-x-4 flex-wrap gap-y-1 justify-end">
+          {revisedAt !== publishedAt && (
+            <div className="flex items-center">
+              <RefreshIcon />
+              <DateTime value={revisedAt} className="ml-2" />
+            </div>
+          )}
+          <div className="flex items-center">
+            <PencilIcon />
+            <DateTime value={publishedAt} className="ml-2" />
           </div>
         </div>
       </CardBody>
