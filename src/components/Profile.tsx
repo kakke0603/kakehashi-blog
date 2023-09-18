1e73aa4a2ba68e4c17b3c4a3b1d7e880a37b2841
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-09-18 16:08:21 +0900

    profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,20 +4,40 @@ import React from "react"
 
 export default function Profile() {
   return (
-    <div className="mt-5">
-      <User
-        name="かっけ"
-        description={
-          <div>
-            ふるすたっくえんじにあ(仮)
-            <br />
-            <Link href="https://twitter.com/kkhs0603" size="sm" isExternal>
-              @kkhs0603
-            </Link>
-          </div>
-        }
-        avatarProps={{ name: "K" }}
-      />
+    <div>
+      <div className="mt-5 flex justify-center">
+        <User
+          name="かっけ"
+          description={
+            <div>
+              ふるすたっくえんじにあ(仮)
+              <br />
+              <Link href="https://twitter.com/kkhs0603" size="sm" isExternal>
+                @kkhs0603
+              </Link>
+            </div>
+          }
+          avatarProps={{ name: "K" }}
+        />
+      </div>
+      <pre className="mt-10">
+        {`\n  
+  { \n
+    Name: "かっけ", \n
+    Age: 31, \n
+    Like: { \n
+      Music: ["back number", "Perfume", "Creepy Nuts", "新しい学校のリーダーズ"] \n
+      Game: ["モンスターハンター"] \n
+      Anime: ["呪術廻戦", "エヴァンゲリオン", "僕のヒーローアカデミア"] \n
+      Movie: ["Marvel", "Star Wars"] \n
+      Technology: ["React", "TypeScript", "javaScript", "AWS", "Laravel", "PHP"] \n
+      YouTube: ["東海オンエア(虫眼鏡さん推し)", "匿名ラジオ", "板橋ハウス"] \n
+      Food: ["麺類全般", "程よく辛いもの", "明太子"] \n
+      Other: ["京都巡り", "カフェ巡り", "旅行"] \n
+  } \n
+  
+  `}
+      </pre>
     </div>
   )
 }
