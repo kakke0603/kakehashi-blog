641453260cb7f0ff44c42353601928b7272e1252
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2023-09-18 23:09:41 +0900

    like

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -32,7 +32,7 @@ export default function Profile() {
       Anime: ["新世紀エヴァンゲリオン", "僕のヒーローアカデミア", "呪術廻戦"], \n
       Movie: ["Marvel", "Star Wars"], \n
       Technology: ["React", "TypeScript", "javaScript", "AWS", "Laravel", "PHP"], \n
-      YouTube: ["東海オンエア(虫眼鏡さん推し)", "匿名ラジオ", "板橋ハウス"], \n
+      YouTube: ["東海オンエア(虫眼鏡さん推し)", "匿名ラジオ", "板橋ハウス", "ねこてん", "オモコロチャンネル"], \n
       Food: ["麺類全般", "程よく辛いもの", "明太子"], \n
       Other: ["京都巡り", "カフェ巡り", "旅行"] \n
     } \n
