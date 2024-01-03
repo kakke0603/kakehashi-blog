1e4b93874fad988fce2d5c82cbff57b677ae3043
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-01-03 15:23:19 +0900

    three fiber camera

--- src/app/playground/threejs/ThreeJs.tsx
+++ src/app/playground/threejs/ThreeJs.tsx
@@ -3,24 +3,16 @@ import type { NextPage } from "next"
 import React, { useRef, useState } from "react"
 import * as THREE from "three"
 import { Canvas, useFrame } from "@react-three/fiber"
+import { CameraControls } from "@react-three/drei"
 
 const ThreeJs: NextPage = () => {
   const Box = (props: JSX.IntrinsicElements["mesh"]) => {
     const ref = useRef<THREE.Mesh>(null!)
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    useFrame((state, delta) => ((ref.current.rotation.x += 0.01), (ref.current.rotation.y += 0.02)))
+    useFrame((state, delta) => ((ref.current.rotation.x += 0.01), (ref.current.rotation.y += 0.01)))
     return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={active ? 2 : 1}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-      >
+      <mesh {...props} ref={ref}>
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? "blue" : "green"} />
+        <meshStandardMaterial color={"white"} wireframe />
       </mesh>
     )
   }
@@ -35,6 +27,7 @@ const ThreeJs: NextPage = () => {
         left: 0,
       }}
     >
+      <CameraControls minPolarAngle={0} />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box position={[0, 0, 0]} />
