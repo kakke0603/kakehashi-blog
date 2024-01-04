5989e04733d2bf7cd856784845c47e143575f9dc
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-01-04 16:16:28 +0900

    env

--- src/components/googleads-script.tsx
+++ src/components/googleads-script.tsx
@@ -1,14 +1,17 @@
-import Script from "next/script"
+import Script from "next/script";
 
-const PUBLISHER_ID = "1124456984547171"
+const PUBLISHER_ID = "1124456984547171";
 
 export const GoogleAdScript = () => {
-  return (
-    <Script
-      async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${PUBLISHER_ID}`}
-      crossOrigin="anonymous"
-      strategy="afterInteractive"
-    />
-  )
-}
+  if (process.env.CLOUDFLARE_ENV === "production") {
+    return (
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${PUBLISHER_ID}`}
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+    );
+  }
+  return <></>;
+};
