75a7cdef0ddaa9a30a1517513b68b442cb672f0c
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-01-16 12:55:07 +0900

    マルしー

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
-import React from "react"
+import React from "react";
 
 export default function Footer() {
-  return <footer className="w-full text-center text-default-500 text-sm">© 2023 かっけ</footer>
+  return <footer className="w-full text-center text-default-500 text-sm">© 2024 かっけ</footer>;
 }
