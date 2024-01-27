a39540c7e60fedcad36f222d0428e16175a3dcfa
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-01-27 13:12:53 +0900

    piano

--- src/app/playground/piano/Piano.tsx
+++ src/app/playground/piano/Piano.tsx
@@ -1,37 +1,27 @@
 "use client";
 import React from "react";
-import { RecoilRoot, atom, useRecoilState } from "recoil";
+import useSound from "use-sound";
 
 export const Piano = () => {
-  const audioHooks = useAudio();
+  const [play, { stop, pause }] = useSound("https://maou.audio/se_inst_piano1_1do/");
 
   return (
     <div>
       <div className="flex relative">
-        <button onClick={() => audioHooks.audio.play()} className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="w-10 h-20 bg-white border"></button>
-        <button className="absolute left-6 w-7 h-10 bg-black z-10"></button>
-        <button className="absolute left-16 w-7 h-10 bg-black z-10"></button>
-        <button className="absolute left-36 w-7 h-10 bg-black z-10"></button>
-        <button className="absolute left-48 w-7 h-10 bg-black z-10"></button>
-        <button className="absolute left-60 w-7 h-10 bg-black z-10"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="w-10 h-20 bg-white border"></button>
+        <button onClick={() => play()} className="absolute left-6 w-7 h-10 bg-black z-10"></button>
+        <button onClick={() => play()} className="absolute left-16 w-7 h-10 bg-black z-10"></button>
+        <button onClick={() => play()} className="absolute left-36 w-7 h-10 bg-black z-10"></button>
+        <button onClick={() => play()} className="absolute left-48 w-7 h-10 bg-black z-10"></button>
+        <button onClick={() => play()} className="absolute left-60 w-7 h-10 bg-black z-10"></button>
       </div>
     </div>
   );
-};
-
-const audioState = atom<HTMLAudioElement>({
-  key: "audio-state",
-  default: typeof window !== "undefined" ? new Audio("./piano.mp3") : undefined,
-});
-
-export const useAudio = () => {
-  const [audio, setAudio] = useRecoilState(audioState);
-  return { audio };
 };
--- src/app/playground/piano/page.tsx
+++ src/app/playground/piano/page.tsx
@@ -1,16 +1,12 @@
-"use client";
 export const runtime = "edge";
 
 import React from "react";
 import { Piano } from "./Piano";
-import { RecoilRoot } from "recoil";
 
 export default async function Page() {
   return (
     <div>
-      <RecoilRoot>
-        <Piano />
-      </RecoilRoot>
+      <Piano />
     </div>
   );
 }
