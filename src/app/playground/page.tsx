7356ea786f7b317d6b407c0e75d2a8a883f7b094
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-01-30 06:41:41 +0900

    alert

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -6,8 +6,17 @@ import ThumOurGoals from "./thum-our-goals.png";
 import ThumThreeJs from "./thum-three-js.png";
 import ThumFramerMotion from "./thum-framer-motion.png";
 import ThumPiano from "./thum-piano.png";
+import ThumAlert from "./thum-alert.png";
 
 const pages = [
+  {
+    id: "alert",
+    image: ThumAlert,
+    title: "alert",
+    content: "evaのアラートっぽいcss",
+    publishedAt: "2024-01-30T02:00:34.925Z",
+    revisedAt: "2024-01-30T02:00:34.925Z",
+  },
   {
     id: "piano",
     image: ThumPiano,
