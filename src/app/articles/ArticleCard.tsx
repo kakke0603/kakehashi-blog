55442483e0051aba154ae4d2e46df3afd82faf4a
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-05 09:49:09 +0900

    break words

--- src/app/articles/ArticleCard.tsx
+++ src/app/articles/ArticleCard.tsx
@@ -1,24 +1,24 @@
-"use client"
-import { DateTime } from "@/components/DateTime"
-import { Card, CardBody } from "@nextui-org/react"
-import { useRouter } from "next/navigation"
-import React from "react"
-import twemoji from "twemoji"
-import { Chip } from "@nextui-org/react"
-import { PencilIcon } from "@/components/Icons/PencilIcon"
-import { RefreshIcon } from "@/components/Icons/RefreshIcon"
-import { ArticleType } from "@/libs/client"
-import { Tag } from "@/components/Tag"
+"use client";
+import { DateTime } from "@/components/DateTime";
+import { Card, CardBody } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import twemoji from "twemoji";
+import { Chip } from "@nextui-org/react";
+import { PencilIcon } from "@/components/Icons/PencilIcon";
+import { RefreshIcon } from "@/components/Icons/RefreshIcon";
+import { ArticleType } from "@/libs/client";
+import { Tag } from "@/components/Tag";
 
 export const ArticleCard: React.FC<ArticleType> = ({ id, title, content, publishedAt, revisedAt, tags }) => {
-  const router = useRouter()
-  const handleClick = () => router.push(`/articles/${id}`)
-  const tag = tags?.[0]?.emoji ?? "📝"
-  const emoji = twemoji.parse(tag)
+  const router = useRouter();
+  const handleClick = () => router.push(`/articles/${id}`);
+  const tag = tags?.[0]?.emoji ?? "📝";
+  const emoji = twemoji.parse(tag);
   return (
     <Card className="py-4 w-full" isPressable onPress={handleClick}>
       <CardBody>
-        <div className="gap-x-4 flex flex-row gap-y-4">
+        <div className="gap-x-4 flex flex-row gap-y-4 w-full">
           <div className="w-[70px] h-[70px] p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
             <div dangerouslySetInnerHTML={{ __html: emoji }}></div>
           </div>
@@ -42,13 +42,13 @@ export const ArticleCard: React.FC<ArticleType> = ({ id, title, content, publish
         </div>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
 function HtmlStringToText({ htmlString }: { htmlString: string }) {
   // ダミーのdiv要素を作成してinnerHTMLを設定し、テキストを取得します。
-  const dummyDiv = document.createElement("div")
-  dummyDiv.innerHTML = htmlString
-  const text = dummyDiv.textContent || dummyDiv.innerText
-  return <div className="block text-sm text-default-500 line-clamp-3 h-[40px]">{text}</div>
+  const dummyDiv = document.createElement("div");
+  dummyDiv.innerHTML = htmlString;
+  const text = dummyDiv.textContent || dummyDiv.innerText;
+  return <div className="block text-sm text-default-500 line-clamp-3 h-[40px] break-all">{text}</div>;
 }
