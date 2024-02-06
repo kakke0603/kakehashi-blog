491c96f707e4734c666446f6420243643168bfcf
kakehashi.shunsuke <kakke0603@gmail.com>
2024-02-06 13:05:07 +0900

    ad

--- src/components/SideAdvertisements.tsx
+++ src/components/SideAdvertisements.tsx
@@ -8,7 +8,7 @@ export const SideAdvertisements = () => {
   const random = Math.floor(Math.random() * 10);
   if (random == 1) {
     return (
-      <>
+      <div>
         {/* wordpress */}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+2Z6SY+5GDG+ZTNGX" rel="nofollow">
           <img
@@ -18,11 +18,11 @@ export const SideAdvertisements = () => {
           />
         </a>
         <img width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z0CMV+2Z6SY+5GDG+ZTNGX" alt="" />
-      </>
+      </div>
     );
   } else if (random == 2) {
     return (
-      <>
+      <div>
         {/* hobby shop metalbox */}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0DF4+EV8PMA+44IY+5ZU29" rel="nofollow">
           <img
@@ -32,11 +32,11 @@ export const SideAdvertisements = () => {
           />
         </a>
         <img width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z0DF4+EV8PMA+44IY+5ZU29" alt=""></img>
-      </>
+      </div>
     );
   } else if (random == 3) {
     return (
-      <>
+      <div>
         {/*  EC site */}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+EI56B6+2BZM+4ATZKX" rel="nofollow">
           <img
@@ -46,11 +46,11 @@ export const SideAdvertisements = () => {
           />
         </a>
         <img width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3Z0E76+EI56B6+2BZM+4ATZKX" alt=""></img>
-      </>
+      </div>
     );
   } else if (random == 4) {
     return (
-      <>
+      <div>
         {/*  動画スタジオ*/}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+EHJQPE+4V0U+ZRALD" rel="nofollow">
           <img
@@ -60,10 +60,10 @@ export const SideAdvertisements = () => {
           />
         </a>
         <img width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z0E76+EHJQPE+4V0U+ZRALD" alt=""></img>
-      </>
+      </div>
     );
   } else if (random == 5) {
-    <>
+    <div>
       {/* MEN'S SELCA */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+E8M8MQ+5EA6+BYDTT" rel="nofollow">
         <img
@@ -73,9 +73,9 @@ export const SideAdvertisements = () => {
         />
       </a>
       <img width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z0E76+E8M8MQ+5EA6+BYDTT" alt=""></img>
-    </>;
+    </div>;
   } else if (random == 6) {
-    <>
+    <div>
       {/* コインランドリー */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+E39C6Q+4RZ4+NTZCH" rel="nofollow">
         <img
@@ -85,9 +85,9 @@ export const SideAdvertisements = () => {
         />
       </a>
       <img width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=3Z0E76+E39C6Q+4RZ4+NTZCH" alt=""></img>
-    </>;
+    </div>;
   } else if (random == 7) {
-    <>
+    <div>
       {/* 女性のためのプロテイン【私の完全美容食】(24-0202) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+E2NWKY+4ZUQ+HVV0H" rel="nofollow">
         <img
@@ -97,9 +97,9 @@ export const SideAdvertisements = () => {
         />
       </a>
       <img width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3Z0E76+E2NWKY+4ZUQ+HVV0H" alt=""></img>
-    </>;
+    </div>;
   } else if (random == 8) {
-    <>
+    <div>
       {/* 他社VPSを凌駕する圧倒的性能！「シン」世代VPS【シン・VPS】(24-0201) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+1VVFQQ+5GDG+NVP2P" rel="nofollow">
         <img
@@ -109,9 +109,9 @@ export const SideAdvertisements = () => {
         />
       </a>
       <img width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=3Z0CMV+1VVFQQ+5GDG+NVP2P" alt=""></img>
-    </>;
+    </div>;
   } else if (random == 9) {
-    <>
+    <div>
       {/* 国内最速！進化の極限を目指したエックスサーバーの新たなカタチ【シン・レンタルサーバー】(24-0201) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+1U34XE+5GDG+609HT" rel="nofollow">
         <img
@@ -121,9 +121,9 @@ export const SideAdvertisements = () => {
         />
       </a>
       <img width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3Z0CMV+1U34XE+5GDG+609HT" alt=""></img>
-    </>;
+    </div>;
   } else {
-    <>
+    <div>
       {/* 性能コスパNo.1のXserverVPSがベース！FX自動売買専用VPS『シン・クラウドデスクトップ for FX』(24-0201) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+1SBLE+5GDG+HWPVL" rel="nofollow">
         <img
@@ -133,6 +133,6 @@ export const SideAdvertisements = () => {
         />
       </a>
       <img width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z0CMV+1SBLE+5GDG+HWPVL" alt=""></img>
-    </>;
+    </div>;
   }
 };
--- src/components/WideAdvertisements.tsx
+++ src/components/WideAdvertisements.tsx
@@ -5,81 +5,81 @@ export const WideAdvertisements = () => {
   const random = Math.floor(Math.random() * 10);
   if (random == 1) {
     return (
-      <>
+      <div>
         {/* wordpress */}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+2Z6SY+5GDG+ZTFR5" rel="nofollow">
           <img width="468" height="60" alt="" src="https://www29.a8.net/svt/bgt?aid=240201463005&wid=001&eno=01&mid=s00000025450006016000&mc=1" />
         </a>
         <img width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3Z0CMV+2Z6SY+5GDG+ZTFR5" alt="" />
-      </>
+      </div>
     );
   } else if (random == 2) {
     return (
-      <>
+      <div>
         {/* hobby shop metalbox */}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0DF4+EV8PMA+44IY+5ZEMP" rel="nofollow">
           <img width="468" height="60" alt="" src="https://www20.a8.net/svt/bgt?aid=240202480899&wid=001&eno=01&mid=s00000019249001005000&mc=1" />
         </a>
         <img width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3Z0DF4+EV8PMA+44IY+5ZEMP" alt="" />
-      </>
+      </div>
     );
   } else if (random == 3) {
     return (
-      <>
+      <div>
         {/*  EC site */}
         <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+EI56B6+2BZM+4ATK5D" rel="nofollow">
           <img width="468" height="60" alt="" src="https://www20.a8.net/svt/bgt?aid=240203490877&wid=001&eno=01&mid=s00000010885026004000&mc=1" />
         </a>
         <img width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3Z0E76+EI56B6+2BZM+4ATK5D" alt="" />
-      </>
+      </div>
     );
   } else if (random == 4) {
-    <>
+    <div>
       {/*  動画スタジオ*/}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+EHJQPE+4V0U+ZQV5T" rel="nofollow">
         <img width="468" height="60" alt="" src="https://www26.a8.net/svt/bgt?aid=240203490876&wid=001&eno=01&mid=s00000022683006004000&mc=1" />
       </a>
       <img width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3Z0E76+EHJQPE+4V0U+ZQV5T" alt="" />
-    </>;
+    </div>;
   } else if (random == 5) {
-    <>
+    <div>
       {/* コインランドリー */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+E39C6Q+4RZ4+NVHCX" rel="nofollow">
         <img width="468" height="60" alt="" src="https://www26.a8.net/svt/bgt?aid=240203490852&wid=001&eno=01&mid=s00000022288004010000&mc=1" />
       </a>
       <img width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3Z0E76+E39C6Q+4RZ4+NVHCX" alt="" />
-    </>;
+    </div>;
   } else if (random == 6) {
-    <>
+    <div>
       {/* 女性のためのプロテイン【私の完全美容食】(24-0202) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0E76+E2NWKY+4ZUQ+HW2Q9" rel="nofollow">
         <img width="468" height="60" alt="" src="https://www27.a8.net/svt/bgt?aid=240203490851&wid=001&eno=01&mid=s00000023309003005000&mc=1" />
       </a>
       <img width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=3Z0E76+E2NWKY+4ZUQ+HW2Q9" alt="" />
-    </>;
+    </div>;
   } else if (random == 7) {
-    <>
+    <div>
       {/* 他社VPSを凌駕する圧倒的性能！「シン」世代VPS【シン・VPS】(24-0201) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+1VVFQQ+5GDG+NVHCX" rel="nofollow">
         <img width="468" height="60" alt="" src="https://www27.a8.net/svt/bgt?aid=240201463114&wid=001&eno=01&mid=s00000025450004010000&mc=1" />
       </a>
       <img width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3Z0CMV+1VVFQQ+5GDG+NVHCX" alt="" />
-    </>;
+    </div>;
   } else if (random == 8) {
-    <>
+    <div>
       {/* 国内最速！進化の極限を目指したエックスサーバーの新たなカタチ【シン・レンタルサーバー】(24-0201) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+1U34XE+5GDG+601S1" rel="nofollow">
         <img width="468" height="60" alt="" src="https://www20.a8.net/svt/bgt?aid=240201463111&wid=001&eno=01&mid=s00000025450001008000&mc=1" />
       </a>
       <img width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3Z0CMV+1U34XE+5GDG+601S1" alt="" />
-    </>;
+    </div>;
   } else {
-    <>
+    <div>
       {/* 性能コスパNo.1のXserverVPSがベース！FX自動売買専用VPS『シン・クラウドデスクトップ for FX』(24-0201) */}
       <a href="https://px.a8.net/svt/ejp?a8mat=3Z0CMV+1SBLE+5GDG+HWI5T" rel="nofollow">
         <img width="468" height="60" alt="" src="https://www28.a8.net/svt/bgt?aid=240201463003&wid=001&eno=01&mid=s00000025450003007000&mc=1" />
       </a>
       <img width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3Z0CMV+1SBLE+5GDG+HWI5T" alt="" />
-    </>;
+    </div>;
   }
 };
