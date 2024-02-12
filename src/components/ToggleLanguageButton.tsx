4384c63cace591913abf510dcf6cd401cd9e63e9
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-12 17:12:03 +0900

    about page

--- src/components/ToggleLanguageButton.tsx
+++ src/components/ToggleLanguageButton.tsx
@@ -1,10 +1,24 @@
 import { Button, Link } from "@nextui-org/react";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { useLang } from "../hooks/useLang";
 
-export default function ToggleLanguageButton({ params: { lang } }) {
+export default function ToggleLanguageButton() {
+  const pathname = usePathname();
+  const { isJapanese } = useLang();
+  const router = useRouter();
+  const url = isJapanese ? pathname.replace("/", "/en/") : pathname.replace("/en/", "/");
   return (
     <div>
-      <Link href={"/"}>ja</Link>/<Link href={"/en"}>en</Link>
+      {isJapanese ? (
+        <Button isIconOnly color="default" onClick={() => router.push(url)} variant="flat">
+          en
+        </Button>
+      ) : (
+        <Button isIconOnly color="default" onClick={() => router.push(url)} variant="flat">
+          ja
+        </Button>
+      )}
     </div>
   );
 }
