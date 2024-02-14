7a40e7a936c8100360f2797b68dd274955dc48af
kakehashi.shunsuke <kakke0603@gmail.com>
2024-02-15 08:49:15 +0900

    page

--- src/app/articles/Articles.tsx
+++ src/app/articles/Articles.tsx
@@ -2,18 +2,25 @@ export const runtime = "edge";
 import { ArticleType, client } from "@/libs/client";
 import React from "react";
 import { ArticleCard } from "./ArticleCard";
+import { Pagination } from "@nextui-org/react";
 
 export default async function Articles() {
-  const data = await client
-    .getAllContents({
-      endpoint: "articles",
-    })
-    .then((res) => res);
+  const list = await client.getList({ endpoint: "articles", queries: { limit: 10 } });
+  const items = await client.get({
+    endpoint: "articles",
+    queries: {
+      limit: 10,
+      offset: 0,
+    },
+  });
   return (
-    <div className="pt-10 px-2 space-y-10  mx-auto">
-      {data?.map((article: ArticleType) => {
+    <div className="pt-10 px-2 space-y-10  mx-auto justify-center">
+      {list.contents?.map((article: ArticleType) => {
         return <ArticleCard {...article} key={article.id} />;
       })}
+      <div className="flex justify-center">
+        <Pagination total={Math.ceil(list.totalCount / 10)} initialPage={1} />
+      </div>
     </div>
   );
 }
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,4 +1,3 @@
-export const runtime = "edge";
 import React from "react";
 import Articles from "./Articles";
 
