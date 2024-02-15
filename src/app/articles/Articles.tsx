428eede56c9413a7659e312f90984699c5a1f534
kakehashi.shunsuke <kakke0603@gmail.com>
2024-02-16 08:13:26 +0900

    articles

--- src/app/articles/Articles.tsx
+++ src/app/articles/Articles.tsx
@@ -1,25 +1,19 @@
+"use client";
 export const runtime = "edge";
 import { ArticleType, client } from "@/libs/client";
-import React from "react";
+import React, { useState } from "react";
 import { ArticleCard } from "./ArticleCard";
 import { Pagination } from "@nextui-org/react";
 
-export default async function Articles() {
-  const list = await client.getList({ endpoint: "articles", queries: { limit: 10 } });
-  const items = await client.get({
-    endpoint: "articles",
-    queries: {
-      limit: 10,
-      offset: 0,
-    },
-  });
+export default function Articles(list: { contents: ArticleType[]; totalCount: number }) {
+  const [offset, setOffset] = useState(0);
   return (
     <div className="pt-10 px-2 space-y-10  mx-auto justify-center">
       {list.contents?.map((article: ArticleType) => {
         return <ArticleCard {...article} key={article.id} />;
       })}
       <div className="flex justify-center">
-        <Pagination total={Math.ceil(list.totalCount / 10)} initialPage={1} />
+        <Pagination total={Math.ceil(list.totalCount / 10)} initialPage={1} onChange={(page) => setOffset(page)} />
       </div>
     </div>
   );
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Articles from "./Articles";
+import { client } from "@/libs/client";
 
 export default async function page() {
+  const list = await client.getList({ endpoint: "articles", queries: { limit: 10, offset: 0 } });
   return (
     <div>
-      <Articles />
+      <Articles {...list} />
     </div>
   );
 }
