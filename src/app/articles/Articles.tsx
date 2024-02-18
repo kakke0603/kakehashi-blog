60c55f5cb84b1028f4f6bcda764312b1ba2ed332
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-18 12:00:19 +0900

    type

--- src/app/articles/Articles.tsx
+++ src/app/articles/Articles.tsx
@@ -1,19 +1,21 @@
 "use client";
 export const runtime = "edge";
 import { ArticleType, client } from "@/libs/client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ArticleCard } from "./ArticleCard";
 import { Pagination } from "@nextui-org/react";
+import { MicroCMSListResponse } from "microcms-js-sdk";
 
 export default function Articles(list: { contents: ArticleType[]; totalCount: number }) {
-  const [offset, setOffset] = useState(0);
+  const [offset, setOffset] = useState(1);
+  const [lists, setList] = useState({ contents: list.contents, totalCount: list.totalCount });
   return (
     <div className="pt-10 px-2 space-y-10  mx-auto justify-center">
-      {list.contents?.map((article: ArticleType) => {
+      {lists.contents?.map((article: ArticleType) => {
         return <ArticleCard {...article} key={article.id} />;
       })}
       <div className="flex justify-center">
-        <Pagination total={Math.ceil(list.totalCount / 10)} initialPage={1} onChange={(page) => setOffset(page)} />
+        <Pagination total={Math.ceil(list.totalCount / 10)} initialPage={offset} onChange={(page) => setOffset(page)} />
       </div>
     </div>
   );
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,13 +1,15 @@
+"use server";
 import React from "react";
 import Articles from "./Articles";
-import { client } from "@/libs/client";
+import { ArticleType, client } from "@/libs/client";
 import { Advertisements } from "@/components/Advertisements";
+import { MicroCMSListResponse } from "microcms-js-sdk";
 
 export default async function page() {
-  const list = await client.getList({ endpoint: "articles", queries: { limit: 10, offset: 0 } });
+  const list: MicroCMSListResponse<ArticleType> = await client.getList({ endpoint: "articles", queries: { limit: 10, offset: 0 } });
   return (
     <div>
-      <Articles {...list} />
+      <Articles contents={list.contents} totalCount={list.totalCount} />
       <Advertisements />
     </div>
   );
