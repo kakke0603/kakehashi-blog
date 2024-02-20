5527e3b02579426b161efd6b2e797be70e537df8
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-20 13:33:20 +0900

    ogp

---
+++ src/app/[en]/articles/[id]/opengraph-image.tsx
@@ -0,0 +1,10 @@
+import { Image } from "@nextui-org/react";
+import React from "react";
+
+export default async function Page() {
+  return (
+    <div>
+      <Image src="./opengraph-image.png" />
+    </div>
+  );
+}
