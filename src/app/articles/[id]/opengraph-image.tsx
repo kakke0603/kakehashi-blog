e79359893edc9aa6bcb247bc71caab27b9f17434
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-20 14:07:33 +0900

    og

--- src/app/articles/[id]/opengraph-image.tsx
+++ src/app/articles/[id]/opengraph-image.tsx
@@ -1,10 +1,10 @@
-import { Image } from "@nextui-org/react";
+import Image from "next/image";
 import React from "react";
 
 export default async function Page() {
   return (
     <div>
-      <Image src="./opengraph-image.png" />
+      <Image src="./opengraph-image.png" alt="kakke-blog" />
     </div>
   );
 }
