8f4932e6607a540b2bef3d1c46ed3c8d290ce141
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-20 12:27:05 +0900

    refactor

--- src/components/Playground/EvaTimer/EvaTimer.tsx
+++ src/components/Playground/EvaTimer/EvaTimer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Button, Link } from "@nextui-org/react";
 import { useTimer } from "use-timer";
@@ -39,13 +39,14 @@ export const EvaTimer = () => {
     }
     setTimerMode("stop");
   };
-  React.useEffect(() => {
+  useEffect(() => {
     if (status === "STOPPED" || status === "PAUSED") return;
     const id = setInterval(() => {
       setRandomNum(Math.floor(Math.random() * 100));
     }, 10);
     return () => clearInterval(id);
   });
+
   return (
     <div className="pt-10">
       <Timer time={time} randomNum={randomNum} timerMode={timerMode} onClick={onClickTimerButton} />
@@ -63,7 +64,6 @@ export const EvaTimer = () => {
         </div>
         <div>制作過程は以下の記事で紹介しています🥳</div>
         <Link href={"https://kakke.site/articles/phth-0wg-1a"}>https://kakke.site/articles/phth-0wg-1a</Link>
-        <div></div>
       </div>
     </div>
   );
