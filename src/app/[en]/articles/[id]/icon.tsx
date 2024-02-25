c4219662da0a53e42e35ee122253d310702642ae
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-26 07:20:33 +0900

    og

---
+++ src/app/[en]/articles/[id]/icon.tsx
@@ -0,0 +1,38 @@
+export const runtime = "edge";
+import { ImageResponse } from "next/og";
+
+export function generateImageMetadata() {
+  return [
+    {
+      contentType: "image/png",
+      size: { width: 48, height: 48 },
+      id: "small",
+    },
+    {
+      contentType: "image/png",
+      size: { width: 72, height: 72 },
+      id: "medium",
+    },
+  ];
+}
+
+export default function Icon({ id }: { id: string }) {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: 88,
+          background: "#000",
+          color: "#fafafa",
+        }}
+      >
+        Icon {id}
+      </div>
+    )
+  );
+}
