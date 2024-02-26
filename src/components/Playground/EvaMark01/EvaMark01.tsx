2d6550d8626f9a164ad239a7216b216376975daa
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-26 11:30:27 +0900

    eye

--- src/components/Playground/EvaMark01/EvaMark01.tsx
+++ src/components/Playground/EvaMark01/EvaMark01.tsx
@@ -20,7 +20,8 @@ export const EvaMark01 = () => {
                 <div className="h-[40px] w-[10px]  bg-purple-500"></div>
                 <div className="h-[40px] w-[10px]  bg-purple-500"></div>
                 <div className="h-[40px] w-[10px]  bg-green-400"></div>
-                <div className="h-[40px] w-[10px]  bg-green-400"></div>
+                <div className="h-[40px] w-[20px]  bg-green-400"></div>
+                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
                 <div className="h-[40px] w-[10px]  bg-purple-500"></div>
               </div>
             </div>
@@ -29,10 +30,33 @@ export const EvaMark01 = () => {
         <div className="flex flex-col absolute top-[150px] left-[295px]">
           <div className="rotate-[80deg] w-[50px] h-[150px] bg-purple-500"></div>
         </div>
-        <div className="flex flex-col absolute top-[130px] left-[350px]">
-          <div className="rotate-[45deg] w-[50px] h-[50px] bg-purple-500"></div>
+        <div className="flex flex-col absolute top-[190px] left-[345px]">
+          <div className="rotate-[45deg] w-[100px] h-[50px] bg-purple-500"></div>
         </div>
-        <div className="rotate-[0deg] w-[50px] h-[50px] bg-green-500"></div>
+        <div className="absolute rotate-[270deg] top-[135px] left-[405px]">
+          <div className="rotate-[0deg] w-[50px] h-[80px] bg-purple-500"></div>
+          <div
+            className="w-0 h-0 
+                border-l-[50px] border-l-transparent 
+                border-t-[20px] border-t-purple-500 
+                border-r-[0px] border-r-transparent"
+          ></div>
+        </div>
+        <div className="absolute rotate-[115deg] top-[228px] left-[260px] w-[50px] h-[50px] bg-purple-500"></div>
+        <div className="absolute rotate-[115deg] top-[258px] left-[291px] w-[50px] h-[20px] bg-purple-800"></div>
+        <div className="absolute rotate-[-15deg] top-[233px] left-[320px] w-[60px] h-[20px] bg-purple-800"></div>
+        <div
+          className="w-0 h-0 absolute rotate-[45deg] top-[260px] left-[320px]
+                border-l-[10px] border-l-transparent 
+                border-t-[30px] border-t-yellow-500 
+                border-r-[10px] border-r-transparent"
+        ></div>
+        <div
+          className="w-0 h-0 absolute rotate-[225deg] top-[240px] left-[340px]
+                border-l-[10px] border-l-transparent 
+                border-t-[30px] border-t-yellow-500 
+                border-r-[10px] border-r-transparent"
+        ></div>
       </div>
     </div>
   );
