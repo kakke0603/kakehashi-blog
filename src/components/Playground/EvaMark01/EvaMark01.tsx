d30382285ce0709628e7cd96e60facecf7a3baa7
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-28 17:20:10 +0900

    front_teeth

--- src/components/Playground/EvaMark01/EvaMark01.tsx
+++ src/components/Playground/EvaMark01/EvaMark01.tsx
@@ -6,6 +6,7 @@ export const EvaMark01 = () => {
       <div className="relative">
         <div className="absolute top-[100px] rotate-[50deg]">
           <div className="flex">
+            {/* 角 */}
             <div
               className="w-0 h-0 
                 border-l-[25px] border-l-transparent 
@@ -27,6 +28,7 @@ export const EvaMark01 = () => {
             </div>
           </div>
         </div>
+        {/* 頭部上部パーツ */}
         <div className="flex flex-col absolute top-[150px] left-[200px]">
           <div className="rotate-[80deg] w-[25px] h-[80px] bg-purple-500"></div>
         </div>
@@ -43,6 +45,7 @@ export const EvaMark01 = () => {
           ></div>
         </div>
         <div className="absolute rotate-[115deg] top-[200px] left-[170px] w-[50px] h-[50px] bg-purple-500"></div>
+        {/* 目 */}
         <div className="absolute rotate-[115deg] top-[220px] left-[180px] w-[50px] h-[10px] bg-purple-800"></div>
         <div className="absolute rotate-[-15deg] top-[192px] left-[210px] w-[60px] h-[10px] bg-purple-800"></div>
         <div
@@ -65,18 +68,28 @@ export const EvaMark01 = () => {
                 border-t-[40px] border-t-purple-500 
                 border-r-[10px] border-r-transparent"
         ></div>
-        <div className="w-[60px] h-[20px] bg-purple-500 absolute rotate-[-35deg] top-[230px] left-[200px]"></div>
+        <div className="w-[60px] h-[22px] bg-purple-500 absolute rotate-[-35deg] top-[230px] left-[200px]"></div>
         <div className="w-[40px] h-[20px] bg-purple-500 absolute rotate-[-55deg] top-[210px] left-[232px]"></div>
+        {/* 上顎 */}
         <div
           className="w-0 h-0 absolute rotate-[80deg] top-[265px] left-[180px]
                 border-l-[10px] border-l-transparent 
                 border-t-[30px] border-t-gray-500 
                 border-r-[10px] border-r-transparent"
         ></div>
-        <div className="w-[10px] h-[50px] bg-gray-500 absolute rotate-[60deg] top-[240px] left-[210px]"></div>
+        <div className="w-[10px] h-[70px] bg-gray-500 absolute rotate-[60deg] top-[224px] left-[221px]"></div>
+        {/* 前歯１ */}
         <div className="w-[10px] h-[20px] bg-gray-400 absolute rotate-[60deg] top-[268px] left-[205px]"></div>
         <div className="w-[15px] h-[15px] bg-gray-400 absolute rotate-[150deg] top-[282px] left-[207px]"></div>
         <div className="w-[15px] h-[15px] bg-gray-400 absolute rotate-[70deg] top-[279px] left-[209px]"></div>
+        {/* 前歯２ */}
+        <div className="w-[30px] h-[20px] bg-gray-400 absolute rotate-[60deg] top-[257px] left-[235px]"></div>
+        <div
+          className="w-0 h-0 absolute rotate-[-30deg] top-[245px] left-[258px]
+                border-l-[0px] border-l-transparent 
+                border-t-[30px] border-t-gray-400 
+                border-r-[10px] border-r-transparent"
+        ></div>
       </div>
     </div>
   );
