43f430810db9c36e17fe236ad7503f9f9ff3846f
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-02-28 16:41:21 +0900

    front_teeth

--- src/components/Playground/EvaMark01/EvaMark01.tsx
+++ src/components/Playground/EvaMark01/EvaMark01.tsx
@@ -9,31 +9,31 @@ export const EvaMark01 = () => {
             <div
               className="w-0 h-0 
                 border-l-[25px] border-l-transparent 
-                border-t-[40px] border-t-purple-500 
+                border-t-[20px] border-t-purple-500 
                 border-r-[0px] border-r-transparent"
             ></div>
-            <div className="h-[40px] w-[300px] bg-purple-500 ">
+            <div className="h-[20px] w-[200px] bg-purple-500 ">
               <div className="flex justify-between">
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
-                <div className="h-[40px] w-[10px]  bg-green-400"></div>
-                <div className="h-[40px] w-[20px]  bg-green-400"></div>
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
-                <div className="h-[40px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-green-400"></div>
+                <div className="h-[20px] w-[20px]  bg-green-400"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
+                <div className="h-[20px] w-[10px]  bg-purple-500"></div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col absolute top-[150px] left-[295px]">
-          <div className="rotate-[80deg] w-[50px] h-[150px] bg-purple-500"></div>
+        <div className="flex flex-col absolute top-[150px] left-[200px]">
+          <div className="rotate-[80deg] w-[25px] h-[80px] bg-purple-500"></div>
         </div>
-        <div className="flex flex-col absolute top-[190px] left-[345px]">
-          <div className="rotate-[45deg] w-[100px] h-[50px] bg-purple-500"></div>
+        <div className="flex flex-col absolute top-[150px] left-[240px]">
+          <div className="rotate-[45deg] w-[50px] h-[50px] bg-purple-500"></div>
         </div>
-        <div className="absolute rotate-[270deg] top-[135px] left-[405px]">
+        <div className="absolute rotate-[270deg] top-[115px] left-[290px]">
           <div className="rotate-[0deg] w-[50px] h-[80px] bg-purple-500"></div>
           <div
             className="w-0 h-0 
@@ -42,21 +42,41 @@ export const EvaMark01 = () => {
                 border-r-[0px] border-r-transparent"
           ></div>
         </div>
-        <div className="absolute rotate-[115deg] top-[228px] left-[260px] w-[50px] h-[50px] bg-purple-500"></div>
-        <div className="absolute rotate-[115deg] top-[258px] left-[291px] w-[50px] h-[20px] bg-purple-800"></div>
-        <div className="absolute rotate-[-15deg] top-[233px] left-[320px] w-[60px] h-[20px] bg-purple-800"></div>
+        <div className="absolute rotate-[115deg] top-[200px] left-[170px] w-[50px] h-[50px] bg-purple-500"></div>
+        <div className="absolute rotate-[115deg] top-[220px] left-[180px] w-[50px] h-[10px] bg-purple-800"></div>
+        <div className="absolute rotate-[-15deg] top-[192px] left-[210px] w-[60px] h-[10px] bg-purple-800"></div>
         <div
-          className="w-0 h-0 absolute rotate-[45deg] top-[260px] left-[320px]
+          className="w-0 h-0 absolute rotate-[45deg] top-[208px] left-[205px]
+                border-l-[10px] border-l-transparent 
+                border-t-[32px] border-t-yellow-500 
+                border-r-[10px] border-r-transparent"
+        ></div>
+        <div
+          className="w-0 h-0 absolute rotate-[230deg] top-[189px] left-[226px]
                 border-l-[10px] border-l-transparent 
                 border-t-[30px] border-t-yellow-500 
                 border-r-[10px] border-r-transparent"
         ></div>
+        <div className="absolute rotate-[155deg] top-[230px] left-[195px] w-[40px] h-[10px] bg-purple-800"></div>
+        <div className="absolute rotate-[125deg] top-[210px] left-[220px] w-[40px] h-[10px] bg-purple-800"></div>
         <div
-          className="w-0 h-0 absolute rotate-[225deg] top-[240px] left-[340px]
+          className="w-0 h-0 absolute rotate-[50deg] top-[250px] left-[180px]
                 border-l-[10px] border-l-transparent 
-                border-t-[30px] border-t-yellow-500 
+                border-t-[40px] border-t-purple-500 
                 border-r-[10px] border-r-transparent"
         ></div>
+        <div className="w-[60px] h-[20px] bg-purple-500 absolute rotate-[-35deg] top-[230px] left-[200px]"></div>
+        <div className="w-[40px] h-[20px] bg-purple-500 absolute rotate-[-55deg] top-[210px] left-[232px]"></div>
+        <div
+          className="w-0 h-0 absolute rotate-[80deg] top-[265px] left-[180px]
+                border-l-[10px] border-l-transparent 
+                border-t-[30px] border-t-gray-500 
+                border-r-[10px] border-r-transparent"
+        ></div>
+        <div className="w-[10px] h-[50px] bg-gray-500 absolute rotate-[60deg] top-[240px] left-[210px]"></div>
+        <div className="w-[10px] h-[20px] bg-gray-400 absolute rotate-[60deg] top-[268px] left-[205px]"></div>
+        <div className="w-[15px] h-[15px] bg-gray-400 absolute rotate-[150deg] top-[282px] left-[207px]"></div>
+        <div className="w-[15px] h-[15px] bg-gray-400 absolute rotate-[70deg] top-[279px] left-[209px]"></div>
       </div>
     </div>
   );
