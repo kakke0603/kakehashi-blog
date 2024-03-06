1fb23afd1a38939bbab331d77b6765a77b530f20
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-03-06 17:53:51 +0900

    side

--- src/components/Playground/EvaMark01/EvaMark01.tsx
+++ src/components/Playground/EvaMark01/EvaMark01.tsx
@@ -32,18 +32,18 @@ export const EvaMark01 = () => {
         <div className="flex flex-col absolute top-[150px] left-[200px]">
           <div className="rotate-[80deg] w-[25px] h-[80px] bg-purple-500"></div>
         </div>
-        <div className="flex flex-col absolute top-[150px] left-[240px]">
-          <div className="rotate-[45deg] w-[50px] h-[50px] bg-purple-500"></div>
+        <div className="flex flex-col absolute top-[140px] left-[240px]">
+          <div className="rotate-[-68deg] w-[66px] h-[66px] bg-purple-500"></div>
         </div>
-        <div className="absolute rotate-[270deg] top-[115px] left-[290px]">
-          <div className="rotate-[0deg] w-[50px] h-[80px] bg-purple-500"></div>
-          <div
-            className="w-0 h-0 
-                border-l-[50px] border-l-transparent 
+        <div className="absolute rotate-[270deg] top-[119px] left-[260px]">
+          <div className="rotate-[-105deg] w-[68px] h-[48px] bg-purple-500"></div>
+        </div>
+        <div
+          className="w-0 h-0 rotate-[-105deg] absolute top-[122px] left-[312px]
+                border-l-[48px] border-l-transparent 
                 border-t-[20px] border-t-purple-500 
                 border-r-[0px] border-r-transparent"
-          ></div>
-        </div>
+        ></div>
         <div className="absolute rotate-[115deg] top-[200px] left-[170px] w-[50px] h-[50px] bg-purple-500"></div>
         {/* 目 */}
         <div className="absolute rotate-[115deg] top-[220px] left-[180px] w-[50px] h-[10px] bg-purple-800"></div>
@@ -70,6 +70,25 @@ export const EvaMark01 = () => {
         ></div>
         <div className="w-[60px] h-[22px] bg-purple-500 absolute rotate-[-35deg] top-[230px] left-[200px]"></div>
         <div className="w-[40px] h-[20px] bg-purple-500 absolute rotate-[-55deg] top-[210px] left-[232px]"></div>
+        {/* サイド部分 */}
+        <div className="w-[40px] h-[16px] bg-purple-500 absolute rotate-[-35deg] top-[197px] left-[255px]">
+          <div className="flex justify-center">
+            <div className="w-[4px] h-[16px] bg-purple-800 rotate-[-20deg]"></div>
+          </div>
+        </div>
+        <div className="w-[80px] h-[16px] bg-purple-500 absolute rotate-[-35deg] top-[204px] left-[252px]">
+          <div className="flex justify-between">
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-800 rotate-[20deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+            <div className="w-[4px] h-[16px] bg-purple-500 rotate-[0deg]"></div>
+          </div>
+        </div>
         {/* 上顎 */}
         <div
           className="w-0 h-0 absolute rotate-[80deg] top-[265px] left-[180px]
