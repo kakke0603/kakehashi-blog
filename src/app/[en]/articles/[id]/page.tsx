5e1b026ab022b6384a74c36cb51887900a483689
kakehashi_shunsuke <kakehashi.shunsuke@happylifecreators.com>
2024-03-14 09:03:36 +0900

    metadata

--- src/app/[en]/articles/[id]/page.tsx
+++ src/app/[en]/articles/[id]/page.tsx
@@ -26,6 +26,44 @@ export async function generateMetadata({ params }: Props, parent: ResolvingMetad
   };
 }
 
+const siteName = "Kakke Blog";
+const description = "This is a blog by kakke";
+const url = "https://kakke.site";
+export const metadata = {
+  title: {
+    default: siteName,
+    /** `next-seo`の`titleTemplate`に相当する機能 */
+    template: `%s - ${siteName}`,
+  },
+  metadataBase: new URL(process.env.URL ?? "http://localhost:3000"),
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: "ja_JP",
+    type: "website",
+  },
+  icons: {
+    icon: "/openGraph-image-square.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteName,
+    description,
+    site: "@kkhs0603",
+    creator: "@kkhs0603",
+    images: ["/openGraph-image-square.png"],
+  },
+  verification: {
+    google: "",
+  },
+  alternates: {
+    canonical: url,
+  },
+};
+
 export default async function Page({ params }: { params: { id: string } }) {
   const data = await getArticle(params.id);
   return (
