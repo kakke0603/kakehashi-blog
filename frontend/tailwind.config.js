b3795e9da4204fa300d77d00abf628fcf94f3d99
kakke0603 <kakke0603@gmail.com>
2024-03-22 18:50:25 +0900

    fix: 🐛 config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 // tailwind.config.js
-const { nextui } = require("@nextui-org/react")
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -14,4 +14,4 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
