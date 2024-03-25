8b71286e4a51aed880152b17f40805711a4f1685
kakke0603 <kakke0603@gmail.com>
2024-03-26 06:31:39 +0900

    feat: 🎸 loader

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { Spinner } from "@nextui-org/react";
+
+const Loader = () => {
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loader;
