b6bdca9cb6b5980b2a2932e9859cb1474a5ced6a
kakke0603 <kakke0603@gmail.com>
2024-03-28 06:04:47 +0900

    fix: 🐛 plans page

--- frontend/src/app/[en]/products/page.tsx
+++ frontend/src/app/[en]/products/page.tsx
@@ -1,21 +1,4 @@
 export const runtime = "edge";
 export default async function Page() {
-  return (
-    <div className="">
-      <div className="">Plan</div>
-      <div className="">無料プラン(0円) </div>
-      <div className="">
-        プレミアムプラン (5000円/月)
-        <div className="">静的サイト(HOMEPAGE)が作れます。</div>
-        <div className="">
-          ブログが作れます
-          <div className="">ブログは更新者が三人</div>
-        </div>
-        <div className="">サーバーのメンテナンス、保守をします</div>
-        <div className="">フォーム(お問い合わせ)ページを作ります</div>
-        <div className="">デザインの改修を行います</div>
-        <div className="">機能の改善・改修を行います</div>
-      </div>
-    </div>
-  );
+  return <div className="">coming soon...</div>;
 }
--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -1,4 +1,10 @@
+import Plans from "../../components/Products/Plans";
+
 export const runtime = "edge";
 export default async function Page() {
-  return <div>Coming soon</div>;
+  return (
+    <div>
+      <Plans />
+    </div>
+  );
 }
