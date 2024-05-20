423daa80c3c4168057caa1d78080cff3f05c8e22
kakehashi.shunsuke <kakke0603@gmail.com>
2024-05-20 16:12:10 +0900

    providers

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -7,7 +7,7 @@ import { I18nProvider } from "../components/I18nProvider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange storageKey="acme-theme">
         <I18nProvider>{children}</I18nProvider>
       </ThemeProvider>
     </NextUIProvider>
